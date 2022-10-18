5c887ca586fc79762945fef14d97fe78f9301476
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-10-18 12:59:46 +0200

    Update grains.py

--- grains.py
+++ grains.py
@@ -1,6 +1,11 @@
 def square(number):
-    pass
+    if(number<1 or number>64):
+        print("Invalid Input")
+    else:
+        return 2**(number-1)
+    
 
 
 def total():
-    pass
+    #using sum of geometric series formula sum = a*(r**n - 1)/(r - 1)
+    return((2**64) - 1)
