e21ba7a14dbecce15c4573efbfcbd3d9c8af7cb5
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2023-01-22 22:30:21 +0100

    Update grains.py

--- grains.py
+++ grains.py
@@ -1,11 +1,21 @@
-def square(number):
-    if(number<1 or number>64):
-        raise ValueError("square must be between 1 and 64")
-    else:
-        return 2**(number-1)
+#def square(number):
+#    if(number<1 or number>64):
+#        raise ValueError("square must be between 1 and 64")
+#    else:
+#        return 2**(number-1)
     
 
 
+#def total():
+#    #using sum of geometric series formula sum = a*(r**n - 1)/(r - 1)
+#    return((2**64) - 1)
+
+def square(number):
+    if (number<1 or number>64):
+        raise ValueError ("square must be between 1 and 64")
+    else:
+        grain = 2**(number-1)
+        return grain
+
 def total():
-    #using sum of geometric series formula sum = a*(r**n - 1)/(r - 1)
-    return((2**64) - 1)
+    return (18446744073709551615)
